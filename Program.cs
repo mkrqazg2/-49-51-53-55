8e228f7abd6c169329d2522a64180bd0d7207e94
Vyacheslav <9366086@inbox.ru>
2022-02-27 14:07:11 +0300

    решение задачи 51

--- Program.cs
+++ Program.cs
@@ -1,33 +1,4 @@
-﻿//Задача 49: Показать двумерный массив размером m×n, заполненный вещественными числами.
-/* void PrintArray(double[,] matr)
-{
-for (double i = 0; i < matr.GetLength(0); i++)
-{
-for (double j = 0; j < matr.GetLength(1); j++)
-{
-Console.Write($" {matr[i, j]}");
-}
-Console.WriteLine();
-}
-}
-
-void FillArray(double[,] matr)
-{
-for (double i = 0; i < matr.GetLength(0); i++)
-{
-for (double j = 0; j < matr.GetLength(1); j++)
-{
-matr[i, j] = new RandomDouble().Next(1, 100) ; // заполняем числами [0,100)
-}
-}
-}
-Console.WriteLine("Введите количество строк ");
-double m = double.Parse(Console.ReadLine());
-Console.WriteLine("Введите количество столбцов ");
-double n = double.Parse(Console.ReadLine());
-double[,] array = new double[m,n];  // m это строки; n столбцы
-FillArray(array);
-PrintArray(array); */
+﻿/* //Задача 49: Показать двумерный массив размером m×n, заполненный вещественными числами.
 
 double[,] a = new double[5, 10];
 
@@ -38,6 +9,19 @@ for (int i = 0; i < a.GetLength(0); i++)
     {
         a[i, j] = random.NextDouble() * 1; // NextDouble() дает случайное вещественное число в диапазоне от 0 до 1
         Console.Write("{0,6:F2}", a[i, j]);
+    }
+    Console.WriteLine();
+} */
+
+//Задача 51: Задать двумерный массив следующим правилом: Aₘₙ = m+n.
+
+int[,] a = new int[5, 6];
+for (int i = 0; i < a.GetLength(0); i++)
+{
+    for (int j = 0; j < a.GetLength(1); j++)
+    {
+        a[i,j]=i+j;
+        Console.Write($" {a[i,j]}");
     }
     Console.WriteLine();
 }
